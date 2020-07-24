ce1f1c55ad271197b553afebbd51b2178e9c680a
Dana Epp <dana@vulscan.com>
2020-07-24 12:43:12 -0700

    Refactored scraper to reduce cognitive complexity

--- codeargos/scraper.py
+++ codeargos/scraper.py
@@ -60,42 +60,59 @@ class Scraper:
 
         links = []
 
-        # Get links to other pages within the app
-        for link in parsed_html.find_all("a", href=True):
-            link_url = link.get('href').strip()
-            if link_url is None:
-                continue
+        try:
+            # Get links to other pages within the app
+            for link in parsed_html.find_all("a", href=True):
+                link_url = link.get('href').strip()
+                if link_url is None:
+                    continue
 
-            parsed_link = urlparse(link_url)
+                parsed_link = urlparse(link_url)
 
-            # Need to account for malformed URLs, ie: http:/singleslashisbad.com
-            if parsed_link.scheme and not parsed_link.netloc:
-                continue
-            elif not parsed_link.scheme and not parsed_link.netloc and parsed_link.path:
-                full_url = urljoin(url, link_url)
-            else:
-                full_url = link_url
+                # Need to account for malformed URLs, ie: http:/singleslashisbad.com
+                if parsed_link.scheme and not parsed_link.netloc:
+                    continue
+                elif not parsed_link.scheme and not parsed_link.netloc and parsed_link.path:
+                    full_url = urljoin(url, link_url)
+                else:
+                    full_url = link_url
 
-            links.append( full_url )
+                links.append( full_url )
 
-        # Get links to javascript files used by the app
-        for script in parsed_html.find_all("script"):
+            # Get links to javascript files used by the app
+            for script in parsed_html.find_all("script"):
 
-            if 'src' in script.attrs:
-                script_url = script['src']
-                parsed_url = urlparse(script_url)
+                if 'src' in script.attrs:
+                    script_url = script['src']
+                    parsed_url = urlparse(script_url)
 
-                # In case we have relative path javascript files
-                if not parsed_url.netloc:
-                    script_url = urljoin(self.url, script_url)
-                
-                links.append( script_url )
+                    # In case we have relative path javascript files
+                    if not parsed_url.netloc:
+                        script_url = urljoin(self.url, script_url)
+                    
+                    links.append( script_url )
+        except Exception as e:
+            logging.exception(e)
 
         return links    
 
+    def check_changes(self, new_sig, content):
+        # Check signature to see if the page has changed 
+        # and if we even have to process the content further
+        new_content = True
+        diff_content = ""
+        if self.old_scraped_page: 
+            if self.old_scraped_page.signature == new_sig:                
+                new_content = False
+            else:
+                msg = "Changes detected on {0}".format(self.url)
+                logging.debug(msg)
+                differ = CodeDiffer(True, CodeDifferMode.UNIFIED)
+                diff_content = differ.diff( self.url, self.old_scraped_page.content, content )
+
+        return new_content, diff_content
+
     def scrape(self):
-        raw_content = ""
-        diff_content = "" 
         parsed_html = ""
         new_page_sig = ""
         session = requests.session()
@@ -130,34 +147,17 @@ class Scraper:
                             # OK, its a local code block. Add it to what we have so we can hash all the blocks
                             # as one entity.
                             raw_content += script.string
-                if raw_content:                                    
-                    new_page_sig = hashlib.sha256(raw_content.encode('utf-8')).hexdigest()                            
-                else:
-                    new_page_sig = "NOSCRIPTS"
+
+                new_page_sig = hashlib.sha256(raw_content.encode('utf-8')).hexdigest() if raw_content else "NOSCRIPTS"
             else:
                 status_code = response.status_code if response else "unknown"
                 logging.debug( "Received error status {0} when fetching {1}".format(status_code, self.url))
         except Exception as e:
             logging.exception(e)
 
-        # Check signature to see if the page has changed 
-        # and if we even have to process the content further
-        new_content = True
-        if self.old_scraped_page: 
-            if self.old_scraped_page.signature == new_page_sig:                
-                new_content = False
-            else:
-                msg = "Changes detected on {0}".format(self.url)
-                logging.debug(msg)
-                differ = CodeDiffer(True, CodeDifferMode.UNIFIED)
-                diff_content = differ.diff( self.url, self.old_scraped_page.content, raw_content )
+        new_content, diff_content = self.check_changes(new_page_sig, raw_content)
         
-        scraped_urls = []    
-        if len(parsed_html) > 0:
-            try:
-                scraped_urls = self.get_links(self.url, parsed_html)
-            except Exception as e:
-                logging.exception(e)
+        scraped_urls = self.get_links(self.url, parsed_html)
 
         session.close()
 
